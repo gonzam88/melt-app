319765102a6e1b4a0682918a02d1576c93dff4ff
gonz <gonzamoiguer@gmail.com>
2019-01-16 21:06:15 -0300

    Added copy paste

--- main.js
+++ main.js
@@ -45,27 +45,70 @@ function createWindow () {
 
   // Other code removed for brevity
 
-  var menu = Menu.buildFromTemplate([
-      {
-          label: 'Menu',
-          submenu: [
-              {label:'Toggle Inspector',
-              accelerator: 'CmdOrCtrl+I',
-              click() {
-                  ToggleDevTools();
-              }
-                },
-              {label:'Quit',
-              click() {
-                        app.quit()
-                    }
-                }
-          ]
-      }
-  ])
-  Menu.setApplicationMenu(menu);
+  const application = {
+      label: 'Application',
+      submenu: [{
+          label:'Toggle Inspector',
+          accelerator: 'CmdOrCtrl+I',
+          click() {
+              ToggleDevTools();
+          }
+      },{
+      label:'Quit',accelerator: "CmdOrCtrl+Q",
+          click() {
+              app.quit()
+          }
+        }
+    ]}
 
-}
+    const edit = {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Undo",
+        accelerator: "CmdOrCtrl+Z",
+        selector: "undo:"
+      },
+      {
+        label: "Redo",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        selector: "redo:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:"
+      },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:"
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:"
+      }
+    ]
+  }
+
+  const template = [
+    application,
+    edit
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
+
+}// create window
 
 
 function ToggleDevTools(){
